db6ecd9ffa02cbc3f830df9d5c65a022a9d3d3d8
alan_omam <omamalan@hotmail.it>
2024-04-14 11:33:16 +0200

    FINE ESERCIZIO ( popolato la tabella del mio database con le reghe generate dal faker)

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -9,32 +9,35 @@ use App\Models\Train; //questo si carica automativcamente quando scrivo "new Tra
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+//devo inserire questo percorso per usare i fakers 
+use Faker\Generator as Faker;
+
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker /*devo anche inserire questo */): void
     {
 
+        for($i = 0; $i < 10; $i++) {
         //creo un nuovo oggetto che conterrà le info delle mie nuove righe
         $newTrain = new Train();
 
-        //popolo una riga della mia tabella rispettando il tipo di dato di ogni colonna.
-        //ovviamente se metto tutto questo dentro un ciclo for con n iterazioni di darà n righe ripetute.
-        //questo ciclo deve iniziare pero sopra la righa " $newTrain = new Train();" e finire sotto la righa "$newTrain->save();"
-        $newTrain->Azienda = "TrainItalia";
-        $newTrain->Stazione_di_partenza = "Bologna";
-        $newTrain->Stazione_di_arrivo = "Milano";
-        $newTrain->Orario_di_partenza = "9:00";
-        $newTrain->Orario_di_arrivo = "10:00";
-        $newTrain->Codice_Treno = 7536;
-        $newTrain->Numero_Carrozze = 8;
-        $newTrain->In_orario = false;
-        $newTrain->Cancellato = false;
+        // riempio le mie righe con dei fakers per categoria, rispettando il tipo di doto di ogni collonna.
+        $newTrain->Azienda = $faker->company() ;
+        $newTrain->Stazione_di_partenza = $faker->city();
+        $newTrain->Stazione_di_arrivo = $faker->city();
+        $newTrain->Orario_di_partenza = $faker->dateTimeBetween('-1 week', '+1 week');
+        $newTrain->Orario_di_arrivo = $faker->dateTimeBetween('+2 week', '+3 week');
+        $newTrain->Codice_Treno =  $faker->unique()->randomNumber(6, true);
+        $newTrain->Numero_Carrozze = $faker->numberBetween(1, 10);
+        $newTrain->In_orario = $faker->boolean();
+        $newTrain->Cancellato = $faker->boolean();
 
         //questo ci serve per salvare i campi e applicare e modifiche
         $newTrain->save();
+        }
     }
 }
 
