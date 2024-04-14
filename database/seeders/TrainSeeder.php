61ae1d3fd96451ebecc0f835afcb7733ae9b4712
alan_omam <omamalan@hotmail.it>
2024-04-14 10:12:30 +0200

    aggiunto il file dei seeders

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,20 @@
+<?php
+ /*Questo file l'ho creato da terminale usando 
+ "php artisan make:seeder TrainSeeder"
+ 
+ NB: è in qeusto file che si popolano le righe delle tabelle */
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
